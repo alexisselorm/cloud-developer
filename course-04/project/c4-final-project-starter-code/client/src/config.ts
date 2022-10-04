b101593e9c59547c3a14d480551a06b330aa41b5
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-04 14:41:39 +0100

    Final touches on backend

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'm2gp7ctelc'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-c2rj3ukj.us.auth0.com',            // Auth0 domain
+  clientId: 'xRuvFvYgMG7dfCvNgPrpE9sFJENPnM58',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
