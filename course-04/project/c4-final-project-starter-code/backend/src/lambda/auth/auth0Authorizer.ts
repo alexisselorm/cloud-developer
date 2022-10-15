f5bfa2dfeec0997de745b2dc926a0eecf476d666
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-15 03:15:17 +0100

    Testing auth variant

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,9 +1,9 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify,decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
+// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
@@ -12,7 +12,10 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-c2rj3ukj.us.auth0.com/.well-known/jwks.json'
+// const jwksUrl = 'https://dev-c2rj3ukj.us.auth0.com/.well-known/jwks.json'
+
+// Trying out with HS256
+const secret = process.env.AUTH0SECRET
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,17 +59,17 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  const response = await Axios.get(jwksUrl);
-  const keys = response.data.keys;
-  const signing_key = keys.find(key => (key.kty==='RSA' && key.use==='sig' && key.kid === jwt.header.kid))
-  const certificate: string = `-----BEGIN CERTIFICATE-----\n${signing_key.x5c[0]}\n-----END CERTIFICATE-----\n`
+  // const response = await Axios.get(jwksUrl);
+  // const keys = response.data.keys;
+  // const signing_key = keys.find(key => (key.kty==='RSA' && key.use==='sig' && key.kid === jwt.header.kid))
+  // const certificate: string = `-----BEGIN CERTIFICATE-----\n${signing_key.x5c[0]}\n-----END CERTIFICATE-----\n`
 
-  return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
+  return verify(token, secret) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
